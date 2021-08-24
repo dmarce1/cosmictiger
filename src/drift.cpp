16575517853d19ed100a68dffaa7a330450e034e
Dominic Marcello <dmarce504@gmail.com>
2021-08-23 19:42:44 -0500

    Added drift timers

--- src/drift.cpp
+++ src/drift.cpp
@@ -3,6 +3,7 @@
 #include <cosmictiger/map.hpp>
 #include <cosmictiger/math.hpp>
 #include <cosmictiger/options.hpp>
+#include <cosmictiger/timer.hpp>
 
 HPX_PLAIN_ACTION (drift);
 
@@ -14,7 +15,8 @@ drift_return drift(double scale, double t, double dt) {
 	for (auto c : hpx_children()) {
 		rfuts.push_back(hpx::async < drift_action > (HPX_PRIORITY_BOOST, c, scale, t, dt));
 	}
-	PRINT( "Drifting on %i\n", hpx_rank());
+	const int nthreads = hpx::thread::hardware_concurrency() - 1;
+	PRINT("Drifting on %i with %i threads\n", hpx_rank(), nthreads);
 	drift_return dr;
 	dr.kin = 0.0;
 	dr.momx = 0.0;
@@ -25,72 +27,75 @@ drift_return drift(double scale, double t, double dt) {
 	mutex_type mutex;
 	std::vector<hpx::future<void>> futs;
 	std::atomic<part_int> next(0);
-	const int nthreads = hpx::thread::hardware_concurrency();
-	for (int proc = 0; proc < nthreads; proc++) {
-		const auto func = [&mutex, &dr, dt, scale, proc, nthreads, do_map, t, &next]() {
-			int64_t flops = 0;
-			const double factor = 1.0 / scale;
-			const double a2inv = 1.0 / sqr(scale);
-			double kin = 0.0;
-			healpix_map this_map;
-			double momx = 0.0;
-			double momy = 0.0;
-			double momz = 0.0;
-			part_int nmapped = 0;
-			part_int begin = (next+=CHUNK_SIZE) - CHUNK_SIZE;
-			while( begin < particles_size()) {
-				part_int end = std::min(begin+CHUNK_SIZE,particles_size());
-				for( part_int i = begin; i < end; i++) {
-					double x = particles_pos(XDIM,i).to_double();
-					double y = particles_pos(YDIM,i).to_double();
-					double z = particles_pos(ZDIM,i).to_double();
-					float vx = particles_vel(XDIM,i);
-					float vy = particles_vel(YDIM,i);
-					float vz = particles_vel(ZDIM,i);
-					kin += 0.5 * sqr(vx,vy,vz) * a2inv;
-					momx += vx;
-					momy += vy;
-					momz += vz;
-					vx *= factor;
-					vy *= factor;
-					vz *= factor;
-					double x0, y0, z0;
-					if( do_map) {
-						x0 = x;
-						y0 = y;
-						z0 = z;
-					}
-					x += double(vx*dt);
-					y += double(vy*dt);
-					z += double(vz*dt);
-					if( do_map) {
-						nmapped += this_map.map_add_particle(x0, y0, z0, x, y, z, vx, vy, vz, t, dt);
-					}
-					constrain_range(x);
-					constrain_range(y);
-					constrain_range(z);
-					particles_pos(XDIM,i) = x;
-					particles_pos(YDIM,i) = y;
-					particles_pos(ZDIM,i) = z;
-					flops += 31;
+	const auto func = [&mutex, &dr, dt, scale, do_map, t, &next]() {
+		int64_t flops = 0;
+		const double factor = 1.0 / scale;
+		const double a2inv = 1.0 / sqr(scale);
+		double kin = 0.0;
+		healpix_map this_map;
+		double momx = 0.0;
+		double momy = 0.0;
+		double momz = 0.0;
+		part_int nmapped = 0;
+		part_int begin = (next+=CHUNK_SIZE) - CHUNK_SIZE;
+		while( begin < particles_size()) {
+			part_int end = std::min(begin+CHUNK_SIZE,particles_size());
+			for( part_int i = begin; i < end; i++) {
+				double x = particles_pos(XDIM,i).to_double();
+				double y = particles_pos(YDIM,i).to_double();
+				double z = particles_pos(ZDIM,i).to_double();
+				float vx = particles_vel(XDIM,i);
+				float vy = particles_vel(YDIM,i);
+				float vz = particles_vel(ZDIM,i);
+				kin += 0.5 * sqr(vx,vy,vz) * a2inv;
+				momx += vx;
+				momy += vy;
+				momz += vz;
+				vx *= factor;
+				vy *= factor;
+				vz *= factor;
+				double x0, y0, z0;
+				if( do_map) {
+					x0 = x;
+					y0 = y;
+					z0 = z;
 				}
-				begin = (next+=CHUNK_SIZE) - CHUNK_SIZE;
+				x += double(vx*dt);
+				y += double(vy*dt);
+				z += double(vz*dt);
+				if( do_map) {
+					nmapped += this_map.map_add_particle(x0, y0, z0, x, y, z, vx, vy, vz, t, dt);
+				}
+				constrain_range(x);
+				constrain_range(y);
+				constrain_range(z);
+				particles_pos(XDIM,i) = x;
+				particles_pos(YDIM,i) = y;
+				particles_pos(ZDIM,i) = z;
+				flops += 31;
 			}
-			if( do_map ) {
-				map_add_map(std::move(this_map));
-			}
-			std::lock_guard<mutex_type> lock(mutex);
-			dr.kin += kin;
-			dr.momx += momx;
-			dr.momy += momy;
-			dr.momz += momz;
-			dr.nmapped += nmapped;
-			dr.flops += flops;
-		};
-		futs.push_back(hpx::async(HPX_PRIORITY_BOOST, func));
+			begin = (next+=CHUNK_SIZE) - CHUNK_SIZE;
+		}
+		if( do_map ) {
+			map_add_map(std::move(this_map));
+		}
+		std::lock_guard<mutex_type> lock(mutex);
+		dr.kin += kin;
+		dr.momx += momx;
+		dr.momy += momy;
+		dr.momz += momz;
+		dr.nmapped += nmapped;
+		dr.flops += flops;
+	};
+	timer tm;
+	tm.start();
+	for (int proc = 0; proc < nthreads; proc++) {
+		futs.push_back(hpx::async(func));
 	}
+	func();
 	hpx::wait_all(futs.begin(), futs.end());
-
+	tm.stop();
+	PRINT("Drift on %i took %e s\n", hpx_rank(), tm.read());
 	for (auto& fut : rfuts) {
 		auto this_dr = fut.get();
 		dr.kin += this_dr.kin;
