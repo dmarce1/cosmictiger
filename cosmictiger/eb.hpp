1a42163230ebfd9a718c7fa4df7d13d1c28861b4
Dominic Marcello <dmarce504@gmail.com>
2021-09-18 11:02:13 -0500

    Working on einsteinboltzmann

---
+++ cosmictiger/eb.hpp
@@ -0,0 +1,16 @@
+/*
+ * eb.hpp
+ *
+ *  Created on: Sep 18, 2021
+ *      Author: dmarce1
+ */
+
+#ifndef EB_HPP_
+#define EB_HPP_
+
+
+
+void eb_compute();
+
+
+#endif /* EB_HPP_ */
--- cosmictiger/options.hpp
+++ cosmictiger/options.hpp
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include <string>
@@ -38,6 +37,12 @@ struct options {
 	double code_to_g;
 	double omega_m;
 	double omega_r;
+	double omega_b;
+	double omega_c;
+	double omega_gam;
+	double omega_nu;
+	double Theta;
+	double Yp;
 	double z0;
 	double z1;
 	double hubble;
@@ -49,45 +54,52 @@ struct options {
 
 	template<class A>
 	void serialize(A&& arc, unsigned) {
-		arc &  cuda;
-		arc &  do_lc;
-		arc &  save_force;
-		arc &  do_power;
-		arc &  do_groups;
-		arc &  do_tracers;
-		arc &  do_slice;
-		arc &  do_views;
-		arc &  twolpt;
+		arc & cuda;
+		arc & do_lc;
+		arc & save_force;
+		arc & do_power;
+		arc & do_groups;
+		arc & do_tracers;
+		arc & do_slice;
+		arc & do_views;
+		arc & twolpt;
 
-		arc &  tracer_count;
-		arc &  parts_dim;
-		arc &  tree_cache_line_size;
-		arc &  part_cache_line_size;
-		arc &  check_num;
-		arc &  check_freq;
-		arc &  max_iter;
-		arc &  lc_min_group;
-		arc &  min_group;
-		arc &  view_size;
-		arc &  slice_res;
-		arc &  lc_map_size;
+		arc & tracer_count;
+		arc & parts_dim;
+		arc & tree_cache_line_size;
+		arc & part_cache_line_size;
+		arc & check_num;
+		arc & check_freq;
+		arc & max_iter;
+		arc & lc_min_group;
+		arc & min_group;
+		arc & view_size;
+		arc & slice_res;
+		arc & lc_map_size;
 
-		arc &  lc_b;
-		arc &  theta;
-		arc &  slice_size;
-		arc &  link_len;
-		arc &  hsoft;
-		arc &  GM;
-		arc &  eta;
-		arc &  code_to_s;
-		arc &  code_to_cm;
-		arc &  code_to_g;
-		arc &  omega_m;
-		arc &  omega_r;
-		arc &  z0;
-		arc &  z1;
-		arc &  hubble;
-		arc &  sigma8;
+		arc & lc_b;
+		arc & theta;
+		arc & slice_size;
+		arc & link_len;
+		arc & hsoft;
+		arc & GM;
+		arc & eta;
+		arc & code_to_s;
+		arc & code_to_cm;
+		arc & code_to_g;
+		arc & omega_m;
+		arc & omega_r;
+		arc & Theta;
+		arc & Yp;
+		arc & z0;
+		arc & z1;
+		arc & hubble;
+		arc & sigma8;
+		arc & omega_b;
+		arc & omega_c;
+		arc & omega_gam;
+		arc & omega_nu;
+		arc & z0;
 
 		arc & config_file;
 		arc & test;
