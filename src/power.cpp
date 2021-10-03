1acc507392a0859f635e73d1f41af7a646d2efa8
Dominic Marcello <dmarce504@gmail.com>
2021-10-03 14:01:18 -0500

    Fixed power freeze

--- src/power.cpp
+++ src/power.cpp
@@ -1,25 +1,26 @@
 /*
-CosmicTiger - A cosmological N-Body code
-Copyright (C) 2021  Dominic C. Marcello
+ CosmicTiger - A cosmological N-Body code
+ Copyright (C) 2021  Dominic C. Marcello
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 
 #include <cosmictiger/domain.hpp>
 #include <cosmictiger/fft.hpp>
 #include <cosmictiger/hpx.hpp>
+#include <cosmictiger/math.hpp>
 #include <cosmictiger/options.hpp>
 #include <cosmictiger/particles.hpp>
 #include <cosmictiger/power.hpp>
@@ -27,7 +28,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 static void compute_density();
 
 HPX_PLAIN_ACTION (compute_density);
-
 
 vector<float> power_spectrum_compute() {
 	const int N = get_options().parts_dim;
@@ -40,30 +40,31 @@ vector<float> power_spectrum_compute() {
 }
 
 static void compute_density() {
-	vector<float> rho;
+	vector<float> rho0;
 	vector<hpx::future<void>> futs1;
 	vector<hpx::future<void>> futs2;
 	for (auto c : hpx_children()) {
-		futs1.push_back(hpx::async < compute_density_action > (c));
+		futs1.push_back(hpx::async<compute_density_action>(c));
 	}
 	const int N = get_options().parts_dim;
 	const auto dblbox = domains_find_my_box();
 	range<int64_t> intbox;
 	for (int dim = 0; dim < NDIM; dim++) {
 		intbox.begin[dim] = dblbox.begin[dim] * N;
-		intbox.end[dim] = dblbox.end[dim] * N + 2;
+		intbox.end[dim] = dblbox.end[dim] * N + 1;
 	}
-	rho.resize(intbox.volume(), 0.0);
+	vector<std::atomic<float>> rho(intbox.volume());
+	for (int i = 0; i < rho.size(); i++) {
+		rho[i] = 0.0;
+	}
 	array<int64_t, NDIM> I;
-	for (I[0] = intbox.begin[XDIM]; I[0] < intbox.end[XDIM] - 2; I[0]++) {
-		for (I[1] = intbox.begin[YDIM]; I[1] < intbox.end[YDIM] - 2; I[1]++) {
-			for (I[2] = intbox.begin[ZDIM]; I[2] < intbox.end[ZDIM] - 2; I[2]++) {
-				rho[intbox.index(I)] -= 1.0;
+	for (I[0] = intbox.begin[XDIM]; I[0] < intbox.end[XDIM] - 1; I[0]++) {
+		for (I[1] = intbox.begin[YDIM]; I[1] < intbox.end[YDIM] - 1; I[1]++) {
+			for (I[2] = intbox.begin[ZDIM]; I[2] < intbox.end[ZDIM] - 1; I[2]++) {
+				rho[intbox.index(I)] = -1.0;
 			}
 		}
 	}
-	static vector<spinlock_type> mutexes(intbox.end[XDIM] - intbox.begin[XDIM]);
-
 	const int nthreads = hpx::thread::hardware_concurrency();
 	for (int proc = 0; proc < nthreads; proc++) {
 		futs2.push_back(hpx::async([proc,nthreads,N,intbox,&rho]() {
@@ -71,12 +72,12 @@ static void compute_density() {
 			const part_int end = (size_t) (proc+1) * particles_size() / nthreads;
 			const float Ninv = 1.0 / N;
 			for( int i = begin; i < end; i++) {
-				const float x = particles_pos(XDIM,i).to_float();
-				const float y = particles_pos(YDIM,i).to_float();
-				const float z = particles_pos(ZDIM,i).to_float();
-				const part_int i0 = x * N;
-				const part_int j0 = y * N;
-				const part_int k0 = z * N;
+				const double x = particles_pos(XDIM,i).to_double();
+				const double y = particles_pos(YDIM,i).to_double();
+				const double z = particles_pos(ZDIM,i).to_double();
+				const part_int i0 = std::min(std::max((int64_t) (x * N), intbox.begin[XDIM]), intbox.end[XDIM] - 2);
+				const part_int j0 = std::min(std::max((int64_t) (y * N), intbox.begin[YDIM]), intbox.end[YDIM] - 2);
+				const part_int k0 = std::min(std::max((int64_t) (z * N), intbox.begin[ZDIM]), intbox.end[ZDIM] - 2);
 				const part_int i1 = i0 + 1;
 				const part_int j1 = j0 + 1;
 				const part_int k1 = k0 + 1;
@@ -102,27 +103,20 @@ static void compute_density() {
 				const part_int i101 = intbox.index(i1,j0,k1);
 				const part_int i110 = intbox.index(i1,j1,k0);
 				const part_int i111 = intbox.index(i1,j1,k1);
-				{
-					if( hpx_rank() == 0 && i0 >= mutexes.size()) {
-						PRINT( "%i %i %e\n", i0, intbox.begin[XDIM], x);
-					}
-					std::lock_guard<spinlock_type> lock(mutexes[i0 - intbox.begin[XDIM]]);
-					rho[i000] += w000;
-					rho[i001] += w001;
-					rho[i010] += w010;
-					rho[i011] += w011;
-				}
-				{
-					std::lock_guard<spinlock_type> lock(mutexes[i1 - intbox.begin[XDIM]]);
-					rho[i100] += w100;
-					rho[i101] += w101;
-					rho[i110] += w110;
-					rho[i111] += w111;
-				}
+				atomic_add(rho[i000], w000);
+				atomic_add(rho[i001], w001);
+				atomic_add(rho[i010], w010);
+				atomic_add(rho[i011], w011);
+				atomic_add(rho[i100], w100);
+				atomic_add(rho[i101], w101);
+				atomic_add(rho[i110], w110);
+				atomic_add(rho[i111], w111);
 			}
 		}));
 	}
 	hpx::wait_all(futs2.begin(), futs2.end());
-	fft3d_accumulate_real(intbox, rho);
+	rho0.resize(rho.size());
+	hpx_copy(PAR_EXECUTION_POLICY, rho.begin(), rho.end(), rho0.begin()).get();
+	fft3d_accumulate_real(intbox, rho0);
 	hpx::wait_all(futs1.begin(), futs1.end());
 }
