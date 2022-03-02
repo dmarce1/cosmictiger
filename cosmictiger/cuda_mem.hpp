30963a863fbd833941460f6f2c720d0af28ebe12
Dominic Marcello <dmarce504@gmail.com>
2022-03-01 20:20:30 -0600

    Working on new memory

---
+++ cosmictiger/cuda_mem.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+/*
+ CosmicTiger - A cosmological N-Body code
+ Copyright (C) 2021  Dominic C. Marcello
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <cosmictiger/cuda.hpp>
+#include <cosmictiger/containers.hpp>
+
+#define CUDA_MEM_STACK_SIZE (32*1024)
+#define CUDA_MEM_NBIN 32
+#define CUDA_MEM_BLOCK_SIZE (65*1024)
+
+class cuda_mem {
+	int* lock;
+	array<array<char*, CUDA_MEM_STACK_SIZE>, CUDA_MEM_NBIN> free_stacks;
+	array<int, CUDA_MEM_NBIN> stack_pointers;
+	char* heap;
+	int next_block;
+	int heap_max;
+
+	__device__
+	void acquire_lock();
+	__device__
+	void release_lock();
+	__device__
+	char* allocate_blocks(int nblocks);
+	__device__
+	void push(int bin, char* ptr);
+	__device__
+	bool create_new_allocations(int bin);
+public:
+
+	__device__
+	void* allocate(size_t sz);
+	__device__
+	void free(void* ptr);
+	cuda_mem(size_t heap_size);
+	~cuda_mem();
+};
+
