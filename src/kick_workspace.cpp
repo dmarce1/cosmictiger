b6086bb9c8c8bf7ef6049ab7f1dc62a485970c02
Dominic Marcello <dmarce504@gmail.com>
2022-05-21 15:46:36 -0500

    Optimizing?

--- src/kick_workspace.cpp
+++ src/kick_workspace.cpp
@@ -86,69 +86,81 @@ void kick_workspace::to_gpu() {
 	};
 	std::unordered_map<int, part_request> part_requests;
 	std::unordered_map<global_part_range, pair<part_int>, global_part_range_hash> part_map;
-	mutex_type part_map_mutex;
+	mutex_type mutex;
 	std::set<tree_id> remote_roots;
 	part_int next_index = 0;
 	part_int part_index = particles_size();
 	vector<hpx::future<void>> futs1;
 	vector<hpx::future<void>> futs2;
 	vector<hpx::future<void>> futs3;
-
+	int nthreads = hpx_size() == 1 ? 1 : hpx::thread::hardware_concurrency();
 	const size_t opartsize = particles_size();
 	const size_t max_parts = 8 * 1024 * 1024;
 	for (int depth = 0; depth < MAX_DEPTH; depth++) {
 		const auto& ids = ids_by_depth[depth];
-		for (int i = 0; i < ids.size(); i++) {
-			if (tree_map.find(ids[i]) == tree_map.end()) {
-				const tree_node* node = tree_get_node(ids[i]);
-				add_tree_node(tree_map, ids[i], next_index);
-				const int rank = node->proc_range.first;
-				if (rank != hpx_rank()) {
-					const auto range = node->part_range;
-					const auto this_count = range.second - range.first;
-					auto& entry = part_requests[rank];
-					entry.data.push_back(range);
-					entry.count += this_count;
-					global_part_range gpr;
-					gpr.rank = rank;
-					gpr.range = range;
-					const size_t nparts = node->nparts();
-					std::unique_lock<mutex_type> lock(part_map_mutex);
-					part_map[gpr].first = part_index;
-					part_map[gpr].second = part_index + nparts;
-					lock.unlock();
-					part_index += nparts;
-					particles_resize(part_index);
-					remote_roots.insert(ids[i]);
-					if (entry.count > max_parts) {
-						auto ptr = std::make_shared < vector<pair<part_int>>>(std::move(entry.data));
-						size_t count = entry.count;
-						entry.fut = entry.fut.then([ptr,rank,count,&part_map,&part_map_mutex](hpx::future<void> fut) {
-							fut.get();
-							auto data = particles_get(rank,*ptr).get();
-							part_int index = 0;
-							std::unique_lock<shared_mutex_type> lock(particles_shared_mutex());
-							for( int k = 0; k < ptr->size(); k++) {
-								global_part_range gpr;
-								gpr.rank = rank;
-								gpr.range = (*ptr)[k];
-								std::unique_lock<mutex_type> lock(part_map_mutex);
-								auto local_range = part_map[gpr];
-								lock.unlock();
-								const auto nparts = local_range.second - local_range.first;
-								for( int dim = 0; dim < NDIM; dim++) {
-									std::memcpy(&particles_pos(dim,local_range.first), &data[count * dim + index], sizeof(fixed32)*nparts);
-								}
-								index += nparts;
+		for (int proc = 0; proc < nthreads; proc++) {
+			futs1.push_back(hpx::async([proc,nthreads,&ids,&tree_map, &mutex,&next_index,&part_requests,&part_map,&part_index,&remote_roots]() {
+				const int b = (size_t) proc * ids.size() / nthreads;
+				const int e = (size_t)(proc + 1) * ids.size() / nthreads;
+				for (int i = b; i < e; i++) {
+					std::unique_lock<mutex_type> lock(mutex);
+					if (tree_map.find(ids[i]) == tree_map.end()) {
+						lock.unlock();
+						const tree_node* node = tree_get_node(ids[i]);
+						lock.lock();
+						int index = next_index;
+						next_index += node->node_count;
+						add_tree_node(tree_map, ids[i], index);
+						const int rank = node->proc_range.first;
+						if (rank != hpx_rank()) {
+							const auto range = node->part_range;
+							const auto this_count = range.second - range.first;
+							auto& entry = part_requests[rank];
+							entry.data.push_back(range);
+							entry.count += this_count;
+							global_part_range gpr;
+							gpr.rank = rank;
+							gpr.range = range;
+							const size_t nparts = node->nparts();
+							part_map[gpr].first = part_index;
+							part_map[gpr].second = part_index + nparts;
+							part_index += nparts;
+							particles_resize(part_index);
+							remote_roots.insert(ids[i]);
+							if (entry.count > max_parts) {
+								auto ptr = std::make_shared < vector<pair<part_int>>>(std::move(entry.data));
+								size_t count = entry.count;
+								entry.fut = entry.fut.then([ptr,rank,count,&part_map,&mutex](hpx::future<void> fut) {
+											fut.get();
+											auto data = particles_get(rank,*ptr).get();
+											part_int index = 0;
+											std::unique_lock<shared_mutex_type> lock(particles_shared_mutex());
+											for( int k = 0; k < ptr->size(); k++) {
+												global_part_range gpr;
+												gpr.rank = rank;
+												gpr.range = (*ptr)[k];
+												std::unique_lock<mutex_type> lock(mutex);
+												auto local_range = part_map[gpr];
+												lock.unlock();
+												const auto nparts = local_range.second - local_range.first;
+												for( int dim = 0; dim < NDIM; dim++) {
+													std::memcpy(&particles_pos(dim,local_range.first), &data[count * dim + index], sizeof(fixed32)*nparts);
+												}
+												index += nparts;
+											}
+										});
+								entry.count = 0;
+								entry.data.resize(0);
 							}
-						});
-						entry.count = 0;
-						entry.data.resize(0);
+						}
 					}
 				}
-			}
+			}));
 		}
+		hpx::wait_all(futs1.begin(),futs1.end());
 	}
+	futs1.resize(0);
+	nthreads = hpx::thread::hardware_concurrency();
 	for (auto i = part_requests.begin(); i != part_requests.end(); i++) {
 		auto ptr = std::make_shared < vector<pair<part_int>>>(std::move(i->second.data));
 		const int rank = i->first;
@@ -180,7 +192,6 @@ void kick_workspace::to_gpu() {
 		auto& node = tree_nodes[i->second];
 		node = *tree_get_node(i->first);
 	}
-	const int nthreads = hpx::thread::hardware_concurrency();
 	for (int proc = 0; proc < nthreads; proc++) {
 		futs2.push_back(hpx::async(HPX_PRIORITY_HI, [proc,nthreads,&tree_map,&tree_nodes, tree_nodes_size]() {
 			for (int i = proc; i < tree_nodes_size; i+=nthreads) {
