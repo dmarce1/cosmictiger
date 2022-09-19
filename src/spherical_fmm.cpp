95cad49deb65b84687a1ec308ebf5e00df71cf2c
Dominic Marcello <dmarce504@gmail.com>
2022-09-19 16:40:47 -0500

    split radix

--- src/spherical_fmm.cpp
+++ src/spherical_fmm.cpp
@@ -13,7 +13,7 @@ constexpr int index(int l, int m) {
 }
 
 template<class T>
-T nonepow(int m) {
+constexpr T nonepow(int m) {
 	return m % 2 == 0 ? T(1) : T(-1);
 }
 
@@ -102,6 +102,7 @@ template<class T, int P>
 spherical_expansion<T, P> spherical_expansion_M2L(const spherical_expansion<T, P - 1>& M, T x, T y, T z) {
 	const auto O = spherical_singular_harmonic<T, P>(x, y, z);
 	spherical_expansion<T, P> L;
+	int count = 0;
 	for (int n = 0; n <= P; n++) {
 		for (int m = 0; m <= n; m++) {
 			L[index(n, m)] = complex<T>(T(0), T(0));
@@ -111,10 +112,12 @@ spherical_expansion<T, P> spherical_expansion_M2L(const spherical_expansion<T, P
 				const int lmax = std::min(k, n + k - m);
 				for (int l = lmin; l <= lmax; l++) {
 					L[index(n, m)] += M(k, l).conj() * O(n + k, m + l);
+					count += 9;
 				}
 			}
 		}
 	}
+//	printf( "%i\n", count);
 	return L;
 }
 
@@ -183,6 +186,7 @@ constexpr float const_cos(float phi) {
 
 template<class T, int N, int radix, int n, int m, int l, int sgn, bool term>
 struct apply_twiddles {
+	static constexpr int nops = apply_twiddles<T, N, radix, n, m + 1, l, sgn, m + 1 == radix>::nops + 8;
 	void operator()(complex<T>& F, const complex<T>* F0) const {
 		constexpr T phi = (sgn < 0 ? -1.0 : 1.0) * 2.0 * M_PI * (((n + l * N / radix) * m) % N) / N;
 		constexpr T s = const_sin(phi);
@@ -196,12 +200,14 @@ struct apply_twiddles {
 
 template<class T, int N, int radix, int n, int m, int l, int sgn>
 struct apply_twiddles<T, N, radix, n, m, l, sgn, true> {
+	static constexpr int nops = 0;
 	void operator()(complex<T>& F, const complex<T>* F0) const {
 	}
 };
 
 template<class T, int N, int radix, int n, int l, int s, int sgn, bool term>
 struct loop_dfts {
+	static constexpr int nops = apply_twiddles<T, N, radix, n, 1, l, sgn, false>::nops + loop_dfts<T, N, radix, n, l + 1, s, sgn, l + 1 == radix>::nops;
 	void operator()(complex<T>* F, const complex<T>* F0) {
 		auto f = F0[0];
 		apply_twiddles<T, N, radix, n, 1, l, sgn, false> tw;
@@ -214,12 +220,14 @@ struct loop_dfts {
 
 template<class T, int N, int radix, int n, int l, int s, int sgn>
 struct loop_dfts<T, N, radix, n, l, s, sgn, true> {
+	static constexpr int nops = 0;
 	void operator()(complex<T>* F, const complex<T>* F0) {
 	}
 };
 
 template<class T, int N, int radix, int n, int s, int sgn, bool term>
 struct fft_helper {
+	static constexpr int nops = loop_dfts<T, N, radix, n, 0, s, sgn, false>::nops + fft_helper<T, N, radix, n + 1, s, sgn, n + 1 == N / radix>::nops;
 	void operator()(complex<T>* F, const complex<T>* F0) const {
 		loop_dfts<T, N, radix, n, 0, s, sgn, false> loop;
 		loop(F, F0 + n * radix);
@@ -230,19 +238,17 @@ struct fft_helper {
 
 template<class T, int N, int radix, int n, int s, int sgn>
 struct fft_helper<T, N, radix, n, s, sgn, true> {
+	static constexpr int nops = 0;
 	void operator()(complex<T>* F, const complex<T>* F0) const {
 	}
 };
 
-template<class T, int N, int sgn, int S = 1, int radix = lcd(N)>
+template<class T, int N, int sgn, int S = 1, int radix = N % 4 == 0 ? 4 : lcd(N)>
 struct ctfft_run {
+	static constexpr int nops = radix * ctfft_run<T, N / radix, sgn, radix * S, lcd(N / radix)>::nops + fft_helper<T, N, radix, 0, S, sgn, false>::nops;
 	void operator()(complex<T>* F) {
-		if (N == 1) {
-			return;
-		}
-
 		for (int n = 0; n < radix; n++) {
-			ctfft_run<T, N / radix, sgn,radix * S,  lcd(N / radix)> func;
+			ctfft_run<T, N / radix, sgn, radix * S, lcd(N / radix)> func;
 			func(F + n * S);
 		}
 		complex<T> F0[N];
@@ -254,42 +260,163 @@ struct ctfft_run {
 	}
 };
 
+template<class T, int N, int sgn, int S, int k, bool term>
+struct radix4_help {
+	static constexpr int nops = radix4_help<T, N, sgn, S, k + 1, k + 1 == N / 4>::nops + 28;
+	void operator()(complex<T>* F, complex<T>* F0) {
+		constexpr complex<T> I(0, sgn);
+		const auto u1 = F0[2 * k];
+		const auto u2 = F0[2 * k + N / 2];
+		const auto tw10 = expc(complex<T>(0, sgn * 2.0 * M_PI * (k) / N));
+		const auto tw30 = expc(complex<T>(0, 3 * sgn * 2.0 * M_PI * (k) / N));
+		const auto t1 = F0[4 * k + 1] * tw10; // 6
+		const auto t2 = F0[4 * k + 3] * tw30; // 6
+		const auto sum = t1 + t2; // 2
+		const auto dif = t1 - t2; // 2
+		F[k * S] = u1 + sum; // 2
+		F[k * S + (N / 2) * S] = u1 - sum; // 2
+		F[k * S + (N / 4) * S] = u2 + I * dif; // 4
+		F[k * S + 3 * (N / 4) * S] = u2 - I * dif; // 4
+		radix4_help<T, N, sgn, S, k + 1, k + 1 == N / 4> next;
+		next(F, F0);
+	}
+};
+
+template<class T, int sgn, int S>
+struct radix4_help<T, 8, sgn, S, 0, false> {
+	static constexpr int nops = radix4_help<T, 8, sgn, S, 1, false>::nops + 16;
+	void operator()(complex<T>* F, complex<T>* F0) {
+		constexpr complex<T> I(0, sgn);
+		const auto u1 = F0[0];
+		const auto u2 = F0[4];
+		const auto t1 = F0[1]; // 6
+		const auto t2 = F0[3]; // 6
+		const auto sum = t1 + t2; // 2
+		const auto dif = t1 - t2; // 2
+		F[0] = u1 + sum; // 2
+		F[4 * S] = u1 - sum; // 2
+		F[2 * S] = u2 + I * dif; // 4
+		F[6 * S] = u2 - I * dif; // 4
+		radix4_help<T, 8, sgn, S, 1, false> next;
+		next(F, F0);
+	}
+};
+
+template<class T, int N, int sgn, int S, int k>
+struct radix4_help<T, N, sgn, S, k, true> {
+	static constexpr int nops = 0;
+	void operator()(complex<T>* F, complex<T>* F0) {
+	}
+};
+
+template<class T, int N, int sgn, int S>
+struct ctfft_run<T, N, sgn, S, 4> {
+	static constexpr int nops = ctfft_run<T, N / 2, sgn, 2 * S>::nops + 2 * ctfft_run<T, N / 4, sgn, 4 * S>::nops + radix4_help<T, N, sgn, S, 0, false>::nops;
+	void operator()(complex<T>* F) {
+		ctfft_run<T, N / 2, sgn, 2 * S> bigfft;
+		ctfft_run<T, N / 4, sgn, 4 * S> smallfft;
+		bigfft(F);
+		smallfft(F + S);
+		smallfft(F + 3 * S);
+		array<complex<T>, N> F0;
+		for (int i = 0; i < N; i++) {
+			F0[i] = F[S * i];
+		}
+		radix4_help<T, N, sgn, S, 0, false> run;
+		run(F, F0.data());
+	}
+};
+
+template<class T, int sgn, int S>
+struct ctfft_run<T, 2, sgn, S, 2> {
+	static constexpr int nops = 4;
+	void operator()(complex<T>* F) {
+		const auto dif = F[0] - F[S];
+		F[0] += F[S];
+		F[S] = dif;
+	}
+};
+
+template<class T, int sgn, int S>
+struct ctfft_run<T, 4, sgn, S, 2> {
+	static constexpr int nops = 16;
+	void operator()(complex<T>* F) {
+		constexpr complex<T> I(T(0), T(-sgn));
+		auto dif = F[0] - F[2 * S];
+		F[0] += F[2 * S];
+		F[2 * S] = dif;
+		const auto sum = F[S] + F[3 * S];
+		dif = F[S] - F[3 * S];
+		auto u1 = F[0];
+		auto u2 = F[2 * S];
+		F[0] = u1 + sum;
+		F[2 * S] = u1 - sum;
+		F[S].real() = u2.real() - sgn * dif.imag();
+		F[S].imag() = u2.imag() + sgn * dif.real();
+		F[3 * S].real() = u2.real() + sgn * dif.imag();
+		F[3 * S].imag() = u2.imag() - sgn * dif.real();
+	}
+};
+
 template<class T, int N, int sgn, int S>
 struct ctfft_run<T, N, sgn, S, 1> {
+	static constexpr int nops = 0;
 	void operator()(complex<T>* F) {
 
 	}
 };
 
+template<class T, int sgn, int S>
+struct ctfft_run<T, 1, sgn, S, 4> {
+	void operator()(complex<T>* F) {
+
+	}
+};
+
+template<class T, int sgn, int S>
+struct ctfft_run<T, 2, sgn, S, 4> {
+	static constexpr int nops = 4;
+	void operator()(complex<T>* F) {
+		const auto dif = F[0] - F[S];
+		F[0] += F[S];
+		F[S] = dif;
+	}
+};
+
 template<class T, int N>
-void ctfft(complex<T>* F, bool inv = false) {
+int ctfft(complex<T>* F, bool inv = false) {
+	int nops = 0;
 	if (inv) {
 		const T ninv = T(1) / N;
 		for (int i = 0; i < N; i++) {
 			F[i] *= ninv;
 		}
+		nops += N;
 	}
-	if( inv ) {
+	if (inv) {
+		nops += ctfft_run<T, N, 1, 1>::nops;
 		ctfft_run<T, N, 1, 1> f;
 		f(F);
 	} else {
+		nops += ctfft_run<T, N, 1, -1>::nops;
 		ctfft_run<T, N, -1, 1> f;
 		f(F);
 
 	}
-
+	return nops;
 }
 
 template<class T, int N, bool INV, int STRIDE = 1, int OFFSET = 0, bool NORMALIZED = false>
 struct FFT {
-	void operator()(array<complex<T>, STRIDE * N>& F) {
-		ctfft<T, N>(F.data(), INV);
+	int operator()(array<complex<T>, STRIDE * N>& F) {
+		return ctfft<T, N>(F.data(), INV);
 	}
 };
 
 template<class T, bool INV, int STRIDE, int OFFSET, bool NORMALIZED>
 struct FFT<T, 1, INV, STRIDE, OFFSET, NORMALIZED> {
-	void operator()(array<complex<T>, STRIDE>& F) {
+	int operator()(array<complex<T>, STRIDE>& F) {
+		return 0;
 	}
 };
 
@@ -337,17 +464,20 @@ void FFT_c2r_symmetric(array<complex<T>, N / 2 + 1>& A, array<complex<T>, N / 2 
 }
 
 template<class T, int N>
-void FFT_spherical_expansion(array<complex<T>, N / 2>& A, array<complex<T>, N / 2>& B) {
+int FFT_spherical_expansion(array<complex<T>, N / 2>& A, array<complex<T>, N / 2>& B) {
+	int nops;
 	array<complex<T>, N> Z;
-	const auto I = complex<T>(T(0), T(1));
+	constexpr auto I = complex<T>(T(0), T(1));
+	nops += 2;
 	Z[0] = A[0] + I * B[0];
 	for (int n = 1; n < N / 2; n++) {
+		nops += 4;
 		Z[n] = A[n] + I * B[n];
 		Z[N - n] = A[n].conj() * nonepow<T>(n) + I * (B[n].conj() * nonepow<T>(n));
 	}
 	Z[N / 2] = 0.0;
 	FFT<T, N, false> fft;
-	fft(Z);
+	nops += fft(Z);
 	for (int n = 0; n < N / 2; n++) {
 		const auto even = T(0.5) * (Z[n] + Z[n + N / 2]);
 		const auto odd = T(0.5) * (Z[n] - Z[n + N / 2]);
@@ -356,20 +486,24 @@ void FFT_spherical_expansion(array<complex<T>, N / 2>& A, array<complex<T>, N / 
 		B[n].imag() = even.imag();
 		B[n].real() = odd.real();
 		B[n] /= I;
+		nops += 9;
 	}
+	return nops;
 }
 
 template<class T, int N>
-void FFT_spherical_expansion_inv(array<complex<T>, N / 2>& A, array<complex<T>, N / 2>& B) {
+int FFT_spherical_expansion_inv(array<complex<T>, N / 2>& A, array<complex<T>, N / 2>& B) {
+	int nops;
 	array<complex<T>, N> Z;
 	const auto I = complex<T>(T(0), T(1));
 	for (int n = 0; n < N / 2; n++) {
 		Z[n] = A[n] + I * B[n];
 		Z[n + N / 2] = A[n].conj() + I * (B[n].conj());
+		nops += 4;
 	}
 //	Z[N / 2] = 0.0;
 	FFT<T, N, true> fft_inv;
-	fft_inv(Z);
+	nops += fft_inv(Z);
 	for (int n = 0; n < N / 2; n++) {
 		const auto sym = T(0.5) * (Z[n] + Z[(N - n) % (N)].conj());
 		const auto ant = T(0.5) * (Z[n] - Z[(N - n) % (N)].conj());
@@ -380,14 +514,17 @@ void FFT_spherical_expansion_inv(array<complex<T>, N / 2>& A, array<complex<T>, 
 			B[n] = sym / I;
 			A[n] = ant;
 		}
+		nops += 9;
 	}
+	return nops;
 }
 
 template<class T, int P>
 spherical_expansion<T, P> fourier_M2L(const spherical_expansion<T, P - 1>& Mx, T x, T y, T z) {
+	int nops = 0;
 	const auto Gx = spherical_singular_harmonic<T, P>(x, y, z);
 	spherical_expansion<T, P> Lx;
-	constexpr int N = (P + 1);
+	static constexpr int N = (P + 1);
 	array<array<complex<T>, N>, 2 * N> Gk;
 	array<array<complex<T>, N>, 2 * N> Mk;
 	array<array<complex<T>, N>, 2 * N> Lk;
@@ -403,21 +540,23 @@ spherical_expansion<T, P> fourier_M2L(const spherical_expansion<T, P - 1>& Mx, T
 		nnn = (2 * N - n) % (2 * N);
 		for (int m = 0; m <= n; m++) {
 			Gk[nnn][m] = Gx(n, -m);
+			nops++;
 		}
 		if (n % 2 == 1) {
-			FFT_spherical_expansion<T, 2 * N>(Gk[lnnn], Gk[nnn]);
+			nops += FFT_spherical_expansion<T, 2 * N>(Gk[lnnn], Gk[nnn]);
 		} else if (n == N - 1) {
-			FFT_spherical_expansion<T, 2 * N>(Gk[nnn], Gk[1]);
+			nops += FFT_spherical_expansion<T, 2 * N>(Gk[nnn], Gk[1]);
 		}
 	}
 	for (int n = 0; n < P; n++) {
 		for (int m = 0; m <= n; m++) {
 			Mk[n][m] = Mx(n, m).conj();
+			nops++;
 		}
 		if (n % 2 == 1) {
-			FFT_spherical_expansion<T, 2 * N>(Mk[n], Mk[n - 1]);
+			nops += FFT_spherical_expansion<T, 2 * N>(Mk[n], Mk[n - 1]);
 		} else if (n == P - 1) {
-			FFT_spherical_expansion<T, 2 * N>(Mk[n], Mk[n + 1]);
+			nops += FFT_spherical_expansion<T, 2 * N>(Mk[n], Mk[n + 1]);
 		}
 	}
 	FFT<T, 2 * N, false> fft;
@@ -427,14 +566,14 @@ spherical_expansion<T, P> fourier_M2L(const spherical_expansion<T, P - 1>& Mx, T
 		for (int n = 0; n < 2 * N; n++) {
 			col[n] = Gk[n][m];
 		}
-		fft(col);
+		nops += fft(col);
 		for (int n = 0; n < 2 * N; n++) {
 			Gk[n][m] = col[n];
 		}
 		for (int n = 0; n < 2 * N; n++) {
 			col[n] = Mk[n][m];
 		}
-		fft(col);
+		nops += fft(col);
 		for (int n = 0; n < 2 * N; n++) {
 			Mk[n][m] = col[n];
 		}
@@ -443,6 +582,7 @@ spherical_expansion<T, P> fourier_M2L(const spherical_expansion<T, P - 1>& Mx, T
 		array<complex<T>, 2 * N> col;
 		for (int n = 0; n < 2 * N; n++) {
 			col[n] = Gk[n][m] * Mk[n][m];
+			nops++;
 		}
 		fft_inv(col);
 		for (int n = 0; n < 2 * N; n++) {
@@ -458,6 +598,7 @@ spherical_expansion<T, P> fourier_M2L(const spherical_expansion<T, P - 1>& Mx, T
 			Lx[index(n, m)] = Lk[(2 * N - n) % (2 * N)][m].conj() * nonepow<T>(m);
 		}
 	}
+//	printf( "%i\n", nops);
 	return Lx;
 
 }
@@ -533,23 +674,8 @@ real test_M2L(real theta = 0.5) {
 }
 
 int main() {
-	printf("%e\n", const_cos(0.0));
-	printf("%e\n", const_cos(M_PI / 2.0));
-	printf("%e\n", const_cos(M_PI / 4.0));
-	printf("%e\n", const_cos(0.0));
-	test_M2L<3>();
-	test_M2L<4>();
-	test_M2L<5>();
-	test_M2L<6>();
+
 	test_M2L<7>();
-	test_M2L<8>();
-	test_M2L<9>();
-	test_M2L<10>();
-	test_M2L<11>();
-	test_M2L<12>();
-	test_M2L<13>();
-	test_M2L<14>();
-	test_M2L<15>();
 	/*
 	 constexpr int N = 10;
 	 vector<complex<real>> A(N);
