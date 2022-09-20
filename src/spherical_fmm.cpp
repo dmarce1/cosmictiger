9b94c4ff222f0df43673099577603a40dd5fc7d8
Dominic Marcello <dmarce504@gmail.com>
2022-09-19 22:53:30 -0500

    rotating m2l

--- src/spherical_fmm.cpp
+++ src/spherical_fmm.cpp
@@ -72,7 +72,20 @@ double Brot(int n, int m, int l) {
 }
 
 template<class T, int P>
-void spherical_swap_xz(spherical_expansion<T, P>& O) {
+void spherical_swap_xz_singular(spherical_expansion<T, P>& O) {
+	const auto A = O;
+	for (int n = 0; n <= P; n++) {
+		for (int m = 0; m <= n; m++) {
+			O[index(n, m)] = 0.0;
+			for (int l = -n; l <= n; l++) {
+				O[index(n, m)] += A(n, l) * Brot(n, m, l);
+			}
+		}
+	}
+}
+
+template<class T, int P>
+void spherical_swap_xz_regular(spherical_expansion<T, P>& O) {
 	const auto A = O;
 	for (int n = 0; n <= P; n++) {
 		for (int m = 0; m <= n; m++) {
@@ -126,7 +139,37 @@ void spherical_expansion_M2M(spherical_expansion<T, P>& M, T x, T y, T z) {
 }
 
 template<class T, int P>
-spherical_expansion<T, P> spherical_expansion_M2L(const spherical_expansion<T, P - 1>& M, T x, T y, T z) {
+void spherical_rotate_z(spherical_expansion<T, P>& O, T phi) {
+	for (int l = 0; l <= P; l++) {
+		for (int m = 0; m <= l; m++) {
+			O[index(l, m)] *= expc(complex<T>(0, -m * phi));
+		}
+	}
+}
+
+template<class T, int P>
+void spherical_rotate_to_z_singular(spherical_expansion<T, P>& O, T x, T y, T z) {
+	const T phi = atan2(y, x);
+	const T theta = atan2(sqrt(x * x + y * y), z);
+	spherical_rotate_z(O, -phi);
+	spherical_swap_xz_singular(O);
+	spherical_rotate_z(O, theta);
+	spherical_swap_xz_singular(O);
+}
+
+template<class T, int P>
+void spherical_inv_rotate_to_z_singular(spherical_expansion<T, P>& O, T x, T y, T z) {
+	const T phi = atan2(y, x);
+	const T theta = atan2(sqrt(x * x + y * y), z);
+	spherical_swap_xz_singular(O);
+	spherical_rotate_z(O, -theta);
+	spherical_swap_xz_singular(O);
+	spherical_rotate_z(O, phi);
+}
+
+
+template<class T, int P>
+spherical_expansion<T, P> spherical_expansion_M2L(spherical_expansion<T, P - 1> M, T x, T y, T z) {
 	const auto O = spherical_singular_harmonic<T, P>(x, y, z);
 	spherical_expansion<T, P> L;
 	int count = 0;
@@ -144,6 +187,60 @@ spherical_expansion<T, P> spherical_expansion_M2L(const spherical_expansion<T, P
 			}
 		}
 	}
+//spherical_inv_rotate_to_z(L, x, y, z);
+
+//	printf( "%i\n", count);
+	return L;
+}
+
+
+template<class T, int P>
+void spherical_rotate_to_z_regular(spherical_expansion<T, P>& O, T x, T y, T z) {
+	const T phi = atan2(x, y);
+	const T theta = atan2(sqrt(x * x + y * y), z);
+	spherical_rotate_z(O, -phi);
+	spherical_swap_xz_regular(O);
+	spherical_rotate_z(O, theta);
+	spherical_swap_xz_regular(O);
+}
+
+template<class T, int P>
+void spherical_inv_rotate_to_z_regular(spherical_expansion<T, P>& O, T x, T y, T z) {
+	const T phi = atan2(x, y);
+	const T theta = atan2(sqrt(x * x + y * y), z);
+	spherical_swap_xz_singular(O);
+	spherical_rotate_z(O, -theta);
+	spherical_swap_xz_singular(O);
+	spherical_rotate_z(O, phi);
+}
+
+double factorial(int n) {
+	return n == 0 ? 1.0 : n * factorial(n - 1);
+}
+
+template<class T, int P>
+spherical_expansion<T, P> spherical_expansion_rot_M2L(spherical_expansion<T, P - 1> M, T x, T y, T z) {
+	spherical_rotate_to_z_regular(M, x, y, z);
+		const auto O = spherical_singular_harmonic<T, P>(0,0, sqrt(x*x+y*y+z*z));
+	//	const auto O = spherical_singular_harmonic<T, P>(x,y,z);
+	spherical_expansion<T, P> L;
+	int count = 0;
+	for (int n = 0; n <= P; n++) {
+		for (int m = 0; m <= n; m++) {
+			L[index(n, m)] = complex<T>(T(0), T(0));
+			const int kmax = std::min(P - n, P - 1);
+			for (int k = 0; k <= kmax; k++) {
+				const int lmin = std::max(-k, -n - k - m);
+				const int lmax = std::min(k, n + k - m);
+				for (int l = lmin; l <= lmax; l++) {
+					L[index(n, m)] += M(k, l).conj() * O(n + k, m + l);
+					count += 9;
+				}
+			}
+		}
+	}
+	spherical_inv_rotate_to_z_regular(L, x, y, z);
+
 //	printf( "%i\n", count);
 	return L;
 }
@@ -193,11 +290,22 @@ real test_M2L(real theta = 0.5) {
 		random_vector(x0, y0, z0);
 		random_unit(x1, y1, z1);
 		random_vector(x2, y2, z2);
+	/*	x0 = 0.0;
+		y0 = 0.0;
+		z0 = 0.0;
+		x1 = 1.0;
+		y1 = 1.0;
+		z1 = 0.0;
+		x2 = 0.0;
+		y2 = 0.0;
+		z2 = 0.0;*/
+
 		x1 /= 0.5 * theta;
 		y1 /= 0.5 * theta;
 		z1 /= 0.5 * theta;
+		x2= y2 = z2 = 0.0;
 		auto M = spherical_regular_harmonic<real, P - 1>(x0, y0, z0);
-		auto L = spherical_expansion_M2L<real, P>(M, x1, y1, z1);
+		auto L = spherical_expansion_rot_M2L<real, P>(M, x1, y1, z1);
 		spherical_expansion_L2L(L, x2, y2, z2);
 		const real dx = (x2 + x1) - x0;
 		const real dy = (y2 + y1) - y0;
@@ -222,64 +330,28 @@ real test_M2L(real theta = 0.5) {
 	return err;
 }
 
-template<class T, int P>
-void spherical_rotate_z(spherical_expansion<T, P>& O, T phi) {
-	for (int l = 0; l <= P; l++) {
-		for (int m = 0; m <= l; m++) {
-			O[index(l, m)] *= expc(complex<T>(0, -m * phi));
-		}
-	}
-}
-
-template<class T, int P>
-void spherical_rotate_to_z(spherical_expansion<T, P>& O, T x, T y, T z) {
-	const T phi = atan2(x, y);
-	const T theta = atan(sqrt(x * x + y * y) / z);
-	printf("%e\n", phi);
-	spherical_rotate_z(O, phi);
-	spherical_swap_xz(O);
-	spherical_rotate_z(O, theta);
-	spherical_swap_xz(O);
-}
-
 int main() {
+
+	test_M2L<5>();
+
 	constexpr int P = 5;
 	spherical_expansion<float, P> O;
-	float x = 0.0;
-	float y = 1.0;
-	float z = 0.0;
+	float x = 1.0;
+	float y = -1.0;
+	float z = 1.0;
 	auto L = spherical_regular_harmonic<float, P>(x, y, z);
 	for (int l = 0; l <= P; l++) {
 		for (int m = 0; m <= l; m++) {
 			printf("%i %i %e %e\n", l, m, L(l, m).real(), L(l, m).imag());
 		}
 	}
-	spherical_rotate_to_z(L, x, y, z);
+	spherical_rotate_to_z_regular(L, x, y, z);
+	spherical_inv_rotate_to_z_regular(L, x, y, z);
 	printf("\n");
 	for (int l = 0; l <= P; l++) {
 		for (int m = 0; m <= l; m++) {
 			printf("%i %i %e %e\n", l, m, L(l, m).real(), L(l, m).imag());
 		}
 	}
-	/*test_M2L<3>();
-	 test_M2L<4>();
-	 test_M2L<5>();
-	 test_M2L<6>();
-	 test_M2L<7>();
-	 test_M2L<8>();
-	 test_M2L<9>();
-	 test_M2L<10>();
-	 /*
-	 constexpr int N = 10;
-	 vector<complex<real>> A(N);
-	 for (int i = 0; i < N; i++) {
-	 A[i].real() = cos(2.0 * M_PI * (i - N / 2) / N);
-	 A[i].imag() = 0.0;
-	 }
-	 FFT<real, N, false> test;
-	 ctfft<real, N>(A);
-	 for (int i = 0; i < N; i++) {
-	 print("%e %e\n", A[i].real(), A[i].imag());
-	 }*/
 
 }
